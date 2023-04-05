a4c6f2e76c1d6dad820350769890f0e11d8f502e
L0rest <jawad.maache@gmail.com>
2023-04-05 19:18:37 +0200

    Distance method + Vectors and AttributesWeights added

--- src/main/java/com/example/javagoat/back/Profile.java
+++ src/main/java/com/example/javagoat/back/Profile.java
@@ -1,5 +1,7 @@
 package com.example.javagoat.back;
 
+import java.util.HashMap;
+
 public class Profile {
 
     public Identity identity;
@@ -8,6 +10,24 @@ public class Profile {
     public Preferences preferences;
     public Passion passion;
 
+    public Vecteur vectProfile;
+    public Vecteur vectPreferences;
+
+    public HashMap<String, Integer> attributesWeights = new HashMap<>() {{
+        put("Size", 10);
+        put("Age", 10);
+        put("HairColor", 10);
+        put("HairLenght", 10);
+        put("HairType", 10);
+        put("Smoker", 10);
+        put("Athlete", 10);
+        put("Feed", 10);
+        put("BodyBuild", 10);
+        put("Ethnicity", 10);
+        put("Sex", 1000);
+        put("Religion", 10);
+
+    }};
 
     // Customised Profile
     public Profile(Identity identity, PhysicalAttributes physicalAttributes, LifeStyle lifeStyle, Preferences preferences, Passion passion) {
@@ -20,6 +40,9 @@ public class Profile {
         if (this.identity.getBsex() == Biology.sex.MALE) {
             this.physicalAttributes.setSize(this.physicalAttributes.getSize() + 15);
         }
+
+        this.vectProfile = new Vecteur(this);
+        this.vectPreferences = new Vecteur(this.getPreferences());
     }
 
     // Random Profile
@@ -33,6 +56,29 @@ public class Profile {
         if (this.identity.getBsex() == Biology.sex.MALE) {
             this.physicalAttributes.setSize(this.physicalAttributes.getSize() + 15);
         }
+
+        this.vectProfile = new Vecteur(this);
+        this.vectPreferences = new Vecteur(this.getPreferences());
+    }
+
+    public float distance(Profile profile) {
+        float distance = Math.abs(profile.vectProfile.getxSize() - this.vectPreferences.getxSize())
+                + Math.abs(profile.vectProfile.getxAge() - this.vectPreferences.getxAge())
+                + Math.abs(profile.vectProfile.getxHairColor() - this.vectPreferences.getxHairColor())
+                + Math.abs(profile.vectProfile.getxHairLenght() - this.vectPreferences.getxHairLenght())
+                + Math.abs(profile.vectProfile.getxHairType() - this.vectPreferences.getxHairType())
+                + Math.abs(profile.vectProfile.getxSmoker() - this.vectPreferences.getxSmoker())
+                + Math.abs(profile.vectProfile.getxAthlete() - this.vectPreferences.getxAthlete())
+                + Math.abs(profile.vectProfile.getxFeed() - this.vectPreferences.getxFeed())
+                + Math.abs(profile.vectProfile.getxBodyBuild() - this.vectPreferences.getxBodyBuild())
+                + Math.abs(profile.vectProfile.getxEthnicity() - this.vectPreferences.getxEthnicity())
+                + Math.abs(profile.vectProfile.getxSex() - this.vectPreferences.getxSex());
+
+        if (profile.getLifeStyle().getLSreligion() != this.getPreferences().getLifestyle().getLSreligion()) {
+            distance ++;
+        }
+
+        return distance;
     }
 
     public Identity getIdentity() {
@@ -75,20 +121,26 @@ public class Profile {
         this.passion = passion;
     }
 
-
-    public static void main(String[] args) {
-
-        // Generation of 500 random profiles
-        for (int i = 0; i < 10000; i++) {
-            Profile p1 = new Profile();
-            System.out.println(p1.getPhysicalAttributes());
-            System.out.println(p1.getPreferences());
-            System.out.println(p1.getPassion());
-            System.out.println(p1.getLifeStyle());
-            System.out.println(p1.getIdentity());
-            System.out.println(" ");
-        }
-
+    public Vecteur getVectProfile() {
+        return vectProfile;
     }
 
+    public void setVectProfile(Vecteur vectProfile) {
+        this.vectProfile = vectProfile;
+    }
+
+    public Vecteur getVectPreferences() {
+        return vectPreferences;
+    }
+
+    public void setVectPreferences(Vecteur vectPreferences) {
+        this.vectPreferences = vectPreferences;
+    }
+
+    public String toString() {
+        return "Size : " + this.getPhysicalAttributes().getSize() +
+                ", hColor : " + this.getPhysicalAttributes().getHairColor() +
+                ", hLenght : " + this.getPhysicalAttributes().getHairLength() +
+                ", hType : " + this.getPhysicalAttributes().getHairType();
+    }
 }
