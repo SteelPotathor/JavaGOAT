0cfb5b222d719d09abf785ac42990e8ad995e563
Mihawk <116311276+OG-Mihawk@users.noreply.github.com>
2023-04-08 22:10:07 +0200

    Adding logo

--- src/main/java/com/example/javagoat/HelloApplication.java
+++ src/main/java/com/example/javagoat/HelloApplication.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class HelloApplication extends Application {
 
@@ -15,8 +14,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        // add css
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
+
         stage.setTitle("Hello!");
         //remove stage button
         stage.initStyle(StageStyle.UNDECORATED);
