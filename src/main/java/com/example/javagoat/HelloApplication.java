5237cb61f78119fe1abb1b812d758e5cff658bb6
Mihawk <116311276+OG-Mihawk@users.noreply.github.com>
2023-04-08 21:32:14 +0200

    Adding default page

--- src/main/java/com/example/javagoat/HelloApplication.java
+++ src/main/java/com/example/javagoat/HelloApplication.java
@@ -4,16 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        // add css
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
         stage.setTitle("Hello!");
+        //remove stage button
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
