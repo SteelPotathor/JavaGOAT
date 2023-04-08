615f9179c5e4b1692213b9701c1c0737b62bd2b8
Mihawk <116311276+OG-Mihawk@users.noreply.github.com>
2023-04-08 23:17:29 +0200

    Adding menu Item

--- src/main/java/com/example/javagoat/HelloController.java
+++ src/main/java/com/example/javagoat/HelloController.java
@@ -3,17 +3,23 @@ package com.example.javagoat;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
+import javax.swing.*;
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Pane panel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onMouseClicked() {
+        System.out.println("Hello World!");
+        System.out.println(panel);
     }
 
 }
