9292a9a642b7ee0b9c868057b708ef04face3aad
Ashura <116311276+OG-Mihawk@users.noreply.github.com>
2023-04-12 11:27:58 +0200

    Linking scene page profile and dashboard

--- src/main/java/com/example/javagoat/HelloController.java
+++ src/main/java/com/example/javagoat/HelloController.java
@@ -1,15 +1,22 @@
 package com.example.javagoat;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import javax.swing.*;
 import javax.swing.text.html.ImageView;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,11 +24,37 @@ public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    private Parent parent;
+    @FXML
     private ImageView cross_exit;
     @FXML
-    private Pane panel;
-    @FXML
     private TableView<String> table;
+    @FXML
+    private Pane profile_page;
+
+    @FXML
+    private Pane dashboard_page;
+
+    @FXML
+    void change_scene_to_page_profile(MouseEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("profile_page.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void change_scene_to_page_dashboard(MouseEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("home.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
     @FXML
     void exit_script() {
         System.exit(0);
