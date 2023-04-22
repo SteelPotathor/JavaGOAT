c4e830c1e53d2ac100e48ddac7b6b018a712483e
Mihawk <116311276+OG-Mihawk@users.noreply.github.com>
2023-04-23 00:03:09 +0200

    Event change windows

--- src/main/java/com/example/javagoat/HelloController.java
+++ src/main/java/com/example/javagoat/HelloController.java
@@ -21,32 +21,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
     @FXML
     private Stage stage;
     @FXML
     private Scene scene;
     @FXML
     private Parent parent;
-    @FXML
-    private ImageView cross_exit;
-    @FXML
-    private TableView<String> table;
-    @FXML
-    private Pane profile_page;
 
-    @FXML
-    private Pane dashboard_page;
-
-    @FXML
-    void change_scene_to_page_profile(MouseEvent event) throws IOException {
-        parent = FXMLLoader.load(getClass().getResource("profile_page.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(parent);
-        stage.setScene(scene);
-        stage.show();
-    }
     @FXML
     void change_scene_to_page_dashboard(MouseEvent event) throws IOException {
         parent = FXMLLoader.load(getClass().getResource("home.fxml"));
@@ -56,12 +38,52 @@ public class HelloController {
         stage.show();
     }
     @FXML
+    void change_scene_to_new_profile(MouseEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("new_profile.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void change_scene_to_page_search(MouseEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("search.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void change_scene_to_page_historic_match(MouseEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("historic_match.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void change_scene_to_page_calendar(MouseEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("calendar.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void change_scene_to_page_events(MouseEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("events.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+
+
+    @FXML
     void exit_script() {
         System.exit(0);
     }
-    @FXML
-    protected void dashboardclick() {
-        System.out.println("Hello World!");
-    }
+
 
 }
