b76a7ea81d572a423400e29306cf5fad5c0506ce
Mihawk <116311276+OG-Mihawk@users.noreply.github.com>
2023-04-23 01:56:33 +0200

    Adding calendar

--- src/main/java/com/example/javagoat/HelloApplication.java
+++ src/main/java/com/example/javagoat/HelloApplication.java
@@ -13,13 +13,20 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        
-        stage.setTitle("Application");
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.setScene(scene);
-        stage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+
+            stage.setTitle("Application");
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+
+        }
+
+
     }
 
     public static void main(String[] args) {
