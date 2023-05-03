f631ecc2f959def6066c77c40e1de304bca78353
L0rest <jawad.maache@gmail.com>
2023-05-03 10:02:14 +0200

    Circles added

--- src/main/java/com/example/javagoat/Matching_Profiles_Controller.java
+++ src/main/java/com/example/javagoat/Matching_Profiles_Controller.java
@@ -1,4 +1,20 @@
 package com.example.javagoat;
 
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
 public class Matching_Profiles_Controller {
+
+    @FXML
+    private Circle circle_profile_picture11;
+    @FXML
+    private Circle circle_profile_picture12;
+    @FXML
+    private Circle circle_profile_picture111;
+    @FXML
+    private Circle circle_profile_picture1111;
+    @FXML
+    private Circle circle_profile_picture;
+    @FXML
+    private Circle circle_profile_picture1;
 }
