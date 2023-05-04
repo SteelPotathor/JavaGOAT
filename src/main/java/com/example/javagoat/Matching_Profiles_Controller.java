aefa9a4fa4e5f3c2c9d4277beb331c75e5875f55
L0rest <jawad.maache@gmail.com>
2023-05-04 11:43:40 +0200

    Images from profiles implemented

--- src/main/java/com/example/javagoat/Matching_Profiles_Controller.java
+++ src/main/java/com/example/javagoat/Matching_Profiles_Controller.java
@@ -1,32 +1,50 @@
 package com.example.javagoat;
 
+import com.example.javagoat.back.ModelMatch;
 import com.example.javagoat.back.Profile;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.util.*;
 
 public class Matching_Profiles_Controller {
 
+    public ModelMatch modelMatch = new ModelMatch();
+
     @FXML
-    private Circle circle_profile_picture11;
+    private Circle circleProfilePictureSE;
     @FXML
-    private Circle circle_profile_picture12;
+    private Circle circleProfilePictureNW;
     @FXML
-    private Circle circle_profile_picture111;
+    private Circle circleProfilePictureSW;
     @FXML
-    private Circle circle_profile_picture1111;
+    private Circle circleProfilePictureS;
     @FXML
-    private Circle circle_profile_picture;
+    private Circle circleProfilePictureCenter;
     @FXML
-    private Circle circle_profile_picture1;
+    private Circle circleProfilePictureNE;
 
     @FXML
     void Initialize() {
-        circle_profile_picture11.setFill(new ImagePattern(new Image(new File("src\\main\\java\\com\\example\\javagoat\\back\\images\\kumalala.jpg").toString())));
+
+    }
+
+    public void set_match(Profile profile) {
+
+        HashMap<Profile, Integer> result_matching = modelMatch.getKNN(profile.getIdentity().getNoId(), 5);
+        List<Circle> circleList = Arrays.asList(circleProfilePictureNE, circleProfilePictureSE, circleProfilePictureS, circleProfilePictureSW, circleProfilePictureNW);
+        try {
+            circleProfilePictureCenter.setFill(new ImagePattern(new Image(profile.getImageView().getImage().getUrl())));
+            int i = 0;
+            for (Profile p : result_matching.keySet()) {
+                circleList.get(i).setFill(new ImagePattern(new Image(p.getImageView().getImage().getUrl())));
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
